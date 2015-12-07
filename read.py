91692808de542098b11d92299ac58dab3fe84558
Jakob Schwehn <schwehn42@gmail.com>
2015-12-07 17:37:36 +0100

    read.py

---
+++ read.py
@@ -0,0 +1,15 @@
+'''
+Den code kannst du in deine Datei kopieren
+und entsprechend anpassen
+'''
+
+file_source = 'C:\\Users\\schwe\\Desktop\\file.xyz' #bei dir hast du die datei ja schon
+#limit = 10 #limit sodass es nicht die 10 Mio zeilen jedesmal nimmt (debug zwecke)
+with open(file_source) as f: #datei oeffnen
+    content = f.readlines() #alle zeielen in array lesen
+for line in content: #array iterieren
+    #limit = limit - 1 #siehe oben
+    #if limit == 0:
+    #    break
+    parts = line.split(' ') #split in x | y | z
+    print 'z: ' + str(parts[2]) #output z
