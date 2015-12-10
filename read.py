13d774497232a4423b039d09ac6853b34b020b0d
Jakob Schwehn <schwehn42@gmail.com>
2015-12-10 18:56:23 +0100

    min/max in new file

--- read.py
+++ read.py
@@ -4,9 +4,13 @@ und entsprechend anpassen
 '''
 
 file_source = 'C:\\Users\\schwe\\Desktop\\file.xyz' #bei dir hast du die datei ja schon
-#limit = 10 #limit sodass es nicht die 10 Mio zeilen jedesmal nimmt (debug zwecke)
+file_write = 'C:\\Users\\schwe\\Desktop\\modified.xyz' #wohin die neue datei soll
+minZ = 1300
+maxZ = 1500
+withinLimit = []
 with open(file_source) as f: #datei oeffnen
     content = f.readlines() #alle zeielen in array lesen
+    f.close()
 for line in content: #array iterieren
     #limit = limit - 1 #siehe oben
     #if limit == 0:
@@ -14,5 +18,12 @@ for line in content: #array iterieren
     parts = line.split(' ') #split in x | y | z
     x = parts[0]
     y = parts[1]
-    z = parts[2]
-    print 'z: ' + str(z) #output z
+    z = parts[2][:-2] #strip \n
+    if float(z) >= 1300 and float(z) <= 1500:
+        withinLimit.append(line)
+
+#write content of withinLimit to new file
+f = open(file_write, 'w')
+for s in withinLimit:
+    f.write(s)
+f.close()
